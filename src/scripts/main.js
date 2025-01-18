88bdf52430d16222c11252f1d4bd43e856aee9b1
artur.manuilenko <artur.manuilenko@yalantis.net>
2025-01-18 19:02:41 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -21,7 +21,22 @@ const food = {
 const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
-  // WRITE YOUR CODE HERE
+  if (Object.keys(data).length === 0) {
+    return;
+  }
+
+  const ul = document.createElement('ul');
+
+  for (const k in data) {
+    const li = document.createElement('li');
+
+    li.textContent = k;
+    ul.appendChild(li);
+    element.appendChild(ul);
+
+    createTree(li, data[k]);
+  }
+  createTree(element, ul);
 }
 
 createTree(tree, food);
